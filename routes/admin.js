822b4f45194c1f473c2df3f4c51a5c3372187af6
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 16:44:27 +0330

    create route delete product

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,6 @@ router.get("/add-product", adminControllers.getAddProduct);
 router.post("/add-product",adminControllers.postProduct)
 
 router.get('/admin', adminControllers.getAdmin)
+
+router.post('/admin/:prodId',adminControllers.deleteProduct)
 module.exports = router;
