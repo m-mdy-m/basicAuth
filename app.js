cd7b00818367c89d26fc4c12c453c8ce3c0a3b94
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 12:23:39 +0330

    Add MongoDB database connection.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose');
 app.set('view engine', 'ejs')
 app.set('views', 'views')
-
 app.use(bodyParser.urlencoded({extended : false}))
+const URL = "mongodb://localhost:27017/basicAuth"
 
-const start = ()=>{
+const start = async ()=>{
     try {
+        await mongoose.connect(URL)
+        console.log('connect database');
         app.listen(3000,()=>{console.log('run server on port 3000');})
     } catch (error) {
         console.log(error);
