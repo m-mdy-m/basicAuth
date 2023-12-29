d17835035f48bd697dc00191964d4a12f8d51d5a
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 18:05:02 +0330

    Added MongoDB session store and express-session middleware.

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const path = require('path')
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-app.use(bodyParser.urlencoded({extended : false}))
-app.use(express.static(path.join(__dirname , 'public')))
-const URL = "mongodb://localhost:27017/basicAuth"
-const shopRoute = require('./routes/shop')
-const adminRoute = require('./routes/admin')
-const authRoute = require('./routes/auth')
-app.use(authRoute)
-app.use(shopRoute)
-app.use(adminRoute)
-const start = async ()=>{
-    try {
-        await mongoose.connect(URL)
-        console.log('connect database');
-        app.listen(3000,()=>{console.log('run server on port 3000');})
-    } catch (error) {
-        console.log(error);
-    }
-}
-start()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const connectMongodbSession = require("connect-mongodb-session")(session);
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+const URL = "mongodb://localhost:27017/basicAuth";
+const store = new connectMongodbSession({
+  uri: URL,
+  collection: "Sessions",
+});
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+const shopRoute = require("./routes/shop");
+const adminRoute = require("./routes/admin");
+const authRoute = require("./routes/auth");
+app.use(authRoute);
+app.use(shopRoute);
+app.use(adminRoute);
+const start = async () => {
+  try {
+    await mongoose.connect(URL);
+    console.log("connect database");
+    app.listen(3000, () => {
+      console.log("run server on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
