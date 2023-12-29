f863a6599e19f9a42339e0502df99cd0a37b24a5
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 12:20:30 +0330

    Changed the Express.js app to use EJS as the view engine and added a body-parser middleware.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(bodyParser.urlencoded({extended : false}))
+
+const start = ()=>{
+    try {
+        app.listen(3000,()=>{console.log('run server on port 3000');})
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
