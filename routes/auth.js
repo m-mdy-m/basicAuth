65a209bad37f5d187da79bf43b16d26d811996b3
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 17:48:18 +0330

    create route signup

--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,6 @@ const router = express.Router()
 const authControllers = require('../controllers/auth')
 router.get('/signup', authControllers.getSignUp)
 
+router.post('/signup', authControllers.postSignUP)
+
 module.exports = router
