001eb99bcd24285977a7e5376171a89a1952912c
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 16:23:16 +0330

    Update the syntax for creating a new product.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,21 +1,21 @@
-const Product = require('../models/Product')
-exports.getAddProduct = (req,res,nxt)=>{
-    res.render('admin/add-product', {
-        title:"ADD-PRODUCT",
-        path: req.path,
-        editing : false,
-    })
-}
-exports.postProduct = async (req,res,nxt)=>{
-    const title = req.body.title
-    const price = req.body.price
-    console.log('title =>', title);
-    console.log('price =>', price);
-    const newP = await Product.create(
-        title,
-        price
-    )
-    await newP.save()
-    console.log("create Product")
-    res.redirect('/')
-}
+const Product = require("../models/Product");
+exports.getAddProduct = (req, res, nxt) => {
+  res.render("admin/add-product", {
+    title: "ADD-PRODUCT",
+    path: req.path,
+    editing: false,
+  });
+};
+exports.postProduct = async (req, res, nxt) => {
+  const title = req.body.title;
+  const price = req.body.price;
+  console.log("title =>", title);
+  console.log("price =>", price);
+  const newP = await Product.create({
+    title,
+    price,
+  });
+  await newP.save();
+  console.log("create Product");
+  res.redirect("/");
+};
