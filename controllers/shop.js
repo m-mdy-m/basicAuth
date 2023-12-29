4421d6b25d1b8c7be2711ac09d3809f82d027134
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 16:32:43 +0330

    Update shop page with product cards

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,10 @@
-exports.getIndex = (req,res,nxt)=>{
+const Product = require('../models/Product')
+exports.getIndex = async (req,res,nxt)=>{
+    const products = await Product.find()
+    console.log('prod->' ,products);
     res.render('shop/index', {
         path : req.path,
         title : "HOME",
+        products,
     })
 }
