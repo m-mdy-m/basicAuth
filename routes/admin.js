9560dc46510d6b61c297094410ca97e608a30ec4
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 16:39:48 +0330

    Add admin routes and views.

--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const adminControllers = require("../controllers/admin");
 router.get("/add-product", adminControllers.getAddProduct);
 router.post("/add-product",adminControllers.postProduct)
+
+router.get('/admin', adminControllers.getAdmin)
 module.exports = router;
