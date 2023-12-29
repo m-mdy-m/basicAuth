7f2458505ea1b46832b51e7908a09b097f3edf19
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 16:16:39 +0330

    Add create product functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,20 @@
+const Product = require('../models/Product')
 exports.getAddProduct = (req,res,nxt)=>{
     res.render('admin/add-product', {
         title:"ADD-PRODUCT",
         path: req.path,
         editing : false,
     })
+}
+exports.postProduct = async (req,res,nxt)=>{
+    const title = req.body.title
+    const price = req.body.price
+
+    const newP = await Product.create(
+        title,
+        price
+    )
+    await newP.save()
+    console.log("create Product")
+    return res.redirect('/')
 }
