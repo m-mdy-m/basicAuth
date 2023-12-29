54bd8270a544ea1703536e0d208ba326bc46a6df
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 16:17:57 +0330

    Added a cursor pointer to the form button in main.css, added a post route for adding a product in admin.js, and removed an empty line at the end of shop.js.

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const adminControllers = require("../controllers/admin");
 router.get("/add-product", adminControllers.getAddProduct);
+router.post("/add-product",adminControllers.postProduct)
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,5 +2,4 @@ const express = require('express')
 const router = express.Router()
 const shopControllers = require('../controllers/shop')
 router.get('/',shopControllers.getIndex )
-
 module.exports = router
