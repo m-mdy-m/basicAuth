acd8deca7cd4723ae673a986cea093ee6588ac88
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 18:48:02 +0330

    add middleware

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const adminControllers = require("../controllers/admin");
-router.get("/add-product", adminControllers.getAddProduct);
-router.post("/add-product",adminControllers.postProduct)
+const isAuth = require('../middleware/auth')
+router.get("/add-product",isAuth, adminControllers.getAddProduct);
+router.post("/add-product",isAuth,adminControllers.postProduct)
 
-router.get('/admin', adminControllers.getAdmin)
+router.get('/admin',isAuth, adminControllers.getAdmin)
 
-router.post('/admin/:prodId',adminControllers.deleteProduct)
+router.post('/admin/:prodId',isAuth,adminControllers.deleteProduct)
 
 
-router.get('/edit-product/:prodId',adminControllers.editProduct)
-router.post('/edit-product/:prodId',adminControllers.postEdit)
+router.get('/edit-product/:prodId',isAuth,adminControllers.editProduct)
+router.post('/edit-product/:prodId',isAuth,adminControllers.postEdit)
 module.exports = router;
