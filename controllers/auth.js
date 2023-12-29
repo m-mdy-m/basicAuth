31597b6c1ff610d9f0c7b3e93a11b93510f2cc1a
mahdi <mahdimamashli1383@gmail.com>
2023-12-29 17:48:41 +0330

    create controllers auth and post signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,24 @@
+const User = require('../models/User')
 exports.getSignUp = (req,res,nxt)=>{
     res.render('auth/signup', {
         title : 'signUp',
         path:req.path,
-        
     })
+}
+exports.postSignUP = async (req,res,nxt)=>{
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    let user = await User.findOne({email})
+    if(user){
+        console.log('email used');
+        return res.redirect('/signup')
+    }
+    user = await User.create({
+        name,
+        email,
+        password
+    })
+    await user.save()
+    res.redirect('/')
 }
